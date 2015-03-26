4a61d841d6d8a08eadb7d6c26e489fb25c1fada0
Abhay Rana <capt.n3m0@gmail.com>
2015-03-27 03:34:02 +0530

    Merges config recursively

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var merge = require('merge');
 var fs = require('fs');
 var path = require('path');
 
-var config = (function(config_file) {
+var config = function(config_file) {
   if(typeof config_file === "undefined"){
     config_file = path.join(__dirname, 'config.yml');
   }
@@ -13,14 +13,13 @@ var config = (function(config_file) {
   try {
     var def = yaml.safeLoad(fs.readFileSync('./default.yml', 'utf8'));
     var patch   = yaml.safeLoad(fs.readFileSync(config_file, 'utf8'));
-    var config = merge(def, patch);
-    return config;
-    console.log(doc);
+    var conf = merge.recursive(def, patch);
+    return conf;
   } catch (e) {
     console.log(e);
     console.log("YAML parser error");
     process.exit(1);
   }
-})();
+};
 
 module.exports = config;
