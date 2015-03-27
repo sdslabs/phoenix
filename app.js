d5d58e940becc9d653ee85689d319d701720beb6
Abhay Rana <capt.n3m0@gmail.com>
2015-03-27 12:53:29 +0530

    Adds phantomjs injection and logging support

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var childProcess = require('child_process');
 var phantomjs = require('phantomjs');
 var binPath = phantomjs.path;
 var config = require('./config')();
-
+var settings = require('./request')(config.request);
+var tmp = require('tmp');
+var fs = require('fs');
 
 redis.on('error', function(error) {
   // We die with redis
@@ -13,20 +15,51 @@ redis.on('error', function(error) {
 })
 
 redis.on('message', function(channel, message) {
-  console.log('Message: ' + message + ' from channel ' + channel);
 
-  var childArgs = [
-    path.join(__dirname, 'phantom.js'),
-    message
-  ]
+  var cleanUp;
 
-  console.log('Starting phantomjs');
+  tmp.dir({
+    dir: path.join(__dirname, 'logs/'),
+    prefix: 'phoenix-',
+    postfix: '',
+    keep: true
+  },function (err, dir){
+    if(err)
+      return false;
 
-  childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-    // done with the request
-    console.log('Phantomjs Closed');
-  })
-})
+    var configPath = path.join(dir, './config.json');
+
+    // Write the request config to the tmp file
+    fs.writeFile(configPath, JSON.stringify(settings), {mode: 0600}, function (err){
+      if(err){
+        console.error("Couldn't write config");
+        return;
+      }
+
+      console.log('Starting phantomjs');
+
+      var childArgs = [
+        path.join(__dirname, 'phantom.js'),
+        configPath
+      ];
+
+      if(config.js){
+        childArgs.push(path.join(__dirname, config.js));
+      }
+
+      console.log(childArgs);
+
+      childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
+        var logPath = path.join(dir, './browser.log'),
+         errLogPath = path.join(dir, './page.log');
+        // done with the request
+        fs.writeFile(logPath, stdout);
+        fs.writeFile(errLogPath, stderr);
+        console.log('Phantomjs Closed');
+      });
+    });
+  });
+});
 
 // channel subscriptions
 redis.subscribe(config.queue);
--- phantom.js
+++ phantom.js
@@ -1,16 +1,48 @@
-var page = require('webpage').create()
-var system = require('system')
-var args = system.args
+var page = require('webpage').create();
+var system = require('system');
+var fs = require('fs');
+var args = system.args;
 
-if(args.length !== 2) {
-  phantom.exit(1)
+console.error = function (){
+  require("system").stderr.write(Array.prototype.join.call(arguments, ' ') + '\n');
+};
+
+if(args.length < 2) {
+  phantom.exit(1);
 }
 
-var url = args[1]
+var configFile = args[1];
+var settings = JSON.parse(fs.read(configFile));
 
-console.log('URL: ' + url)
+console.log("URL:\t\t" + settings.url);
 
-page.open(url, function(status) {
-  console.log("Status: " + status)
-  phantom.exit(0)
+if(args[2]){
+  console.log("INJECT:\t\t"+args[2]);
+  page.onInitialized = function (){
+    page.injectJs(args[2]);
+  };
+};
+
+page.onConsoleMessage = function (msg){
+  console.error(msg);
+};
+
+page.onAlert = function (msg){
+  console.error("ALERT:\t\t"+msg)
+}
+
+page.onPrompt = function(msg, defaultVal) {
+  console.error("PROMPT:\t\t"+msg);
+  return defaultVal || null;
+};
+
+page.onConfirm = function(msg) {
+  console.error("CONFIRM:\t\t"+msg);
+  return false;//We always click the cancel button
+};
+
+
+page.open(settings.url, settings, function (status){
+  console.log("STATUS:\t\t" + status);
+  phantom.exit(0);
 });
